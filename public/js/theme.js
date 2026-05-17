ea72c7e380813fd147312e290baa21873c38cf64
gitagyabhardwaj <gitagyabhardwaj@gmail.com>
2026-05-17 10:24:42 +0530

    hope

---
+++ public/js/theme.js
@@ -0,0 +1,19 @@
+function toggleDark() {
+  const html = document.documentElement;
+
+  if (html.getAttribute('data-theme') === 'dark') {
+    html.removeAttribute('data-theme');
+    localStorage.setItem('theme', 'light');
+  } else {
+    html.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
+window.onload = () => {
+  const savedTheme = localStorage.getItem('theme');
+
+  if (savedTheme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+};
